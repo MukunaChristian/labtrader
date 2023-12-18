b6b422394088c094cc5c2538d514c21a3a90eeac
wian2 <wian@double-eye.com>
2023-12-18 09:41:26 +0200

    last touches

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -94,7 +94,7 @@ export const Dashboard = () => {
   return (
     <div className="pb-4 bg-light-grey">
       <FilterSideBar setIsFilterSideBarOpen={setIsFilterSideBarOpen} isFilterSideBarOpen={isFilterSideBarOpen} />
-      <div className="border-0 pt-16 mx-6">
+      <div className="border-0 mx-6">
         <Header title='Lab Grown Diamonds' results={diamondAmount} />
         <FilterBar setIsFilterSideBarOpen={setIsFilterSideBarOpen} setCurrentRows={setCurrentRows} currentRows={currentRows} />
         <CustomDataTable currentRows={currentRows} />
