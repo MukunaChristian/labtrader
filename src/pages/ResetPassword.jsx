942399772de18e5967621d583ff2f6401b41fd82
Codi <codi@trixta.com>
2024-01-18 10:07:08 +0000

    Updated login pages

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -7,6 +7,7 @@ export const ResetPassword = () => {
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const [invalidPassword, setinvalidPassword] = useState(false);
+  const [error, setError] = useState("");
 
   const token = searchParams.get('token');
 
@@ -15,9 +16,18 @@ export const ResetPassword = () => {
     const password = e.target["password"].value.trim();
     const confirm_password = e.target["confirm_password"].value.trim();
 
+    if (!password) {
+      setError("Please enter your password");
+      return
+    }
+
+    if (!confirm_password) {
+      setError("Please confirm your password");
+      return
+    }
+
     if (password !== confirm_password) {
-      console.error("Passwords do not match");
-      setinvalidPassword(true)
+      setError("Passwords do not match");
       return;
     }
 
@@ -31,15 +41,16 @@ export const ResetPassword = () => {
   }
 
   return (
-    <div className="h-[100vh] overflow-hidden w-full flex justify-center bg-light-grey">
-      <div className="relative w-[24rem] h-[20rem] border-[1.5px] flex flex-col items-center border-solid border-text my-auto p-5 shadow-lg">
+    <div className="w-full h-full flex items-center justify-center flex-col login-color">
+      <img className="w-[35rem] pb-10" src="src/assets/labtrader-login.png"></img>
+      <div className="relative w-[25rem] flex flex-col items-center p-5 text-white">
         <ArrowLeftIcon className="h-8 w-8 absolute top-5 left-5 cursor-pointer hover:bg-grey/40 rounded-md" onClick={() => { navigate("/login") }} />
         <p className="text-3xl mb-6">Reset Password</p>
-        {invalidPassword && <p className="text-red-400 text-sm">Passwords do not match</p>}
+        {error && <p className="text-red-500 text-med">{error}</p>}
         <form className="mb-4" onSubmit={(e) => { handleResetPassword(e) }}>
           <input name="password" className="default-input mt-5" type="password" placeholder="Password" />
           <input name="confirm_password" className="default-input mt-5" type="password" placeholder="Confirm Password" />
-          <button type="submit" className="w-full h-10 bg-black text-white rounded-md mt-5 login-button">Submit</button>
+          <button type="submit" className="w-full h-10 bg-light-grey text-black rounded-md mt-5 login-button">Submit</button>
         </form>
       </div>
     </div>
